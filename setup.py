fcf131615823ab859f89b247415573b4ada60d5d
Chris Guidry <chris@theguidrys.us>
2015-06-19 21:27:37 -0400

    More friendly requirements.

--- setup.py
+++ setup.py
@@ -7,6 +7,10 @@ __version__ = ''
 with open(os.path.join(os.path.dirname(__file__), 'skim', 'version.py')) as version_file:
     exec(version_file.read())
 
+with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as requirements_file:
+    requires = [line for line in map(str.strip, requirements_file.readlines())
+                if line and not line.startswith('#')]
+
 setup(
     name='skim',
     description='skim, a self-hosted news reader',
@@ -15,20 +19,5 @@ setup(
     author_email='chris@theguidrys.us',
     version=__version__,
     packages=find_packages(),
-    install_requires=[
-        'beautifulsoup4==4.3.2',
-        'cssmin==0.2.0',
-        'Flask==0.10.1',
-        'Flask-Assets==0.10',
-        'flask-debugtoolbar==0.9.2',
-        'feedparser==5.2.0.post1',
-        'html2text==2015.4.14',
-        'html5lib==0.999',
-        'lxml==3.4.4',
-        'Markdown==2.6',
-        'python-slugify==1.0.2',
-        'pyembed==1.1.2',
-        'pygments==2.0.2',
-        'Whoosh==2.7.0'
-    ]
+    install_requires=requires
 )
